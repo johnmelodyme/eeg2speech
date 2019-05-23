308708a00c30af221b05ebb4a119142508693786
John Melody Melissa <Johnmelodyme@yandex.com>
2019-05-23 14:59:43 +0800

    Create example.py

---
+++ example.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# EEG to speech (non-invasive)
+# Excerpt by John Melody Melissa Eskholazht
+import os as M
+import re
+import time as T
+
+# Load the DATA:
+userFileDir = input("Please Do Enter The File Directory:  ")
+
+assert M.path.exists(userFileDir), "Try Again, please. " +str(userFileDir)
+newData = open(userFileDir, "r+")
+existingData = open("/home/johnmelodyme/Documents/API/AI/Data/JMEOG_FWRD_3EB.txt", "r+")
+# Matching Data 
+for line1 in newData:
+        for line2 in existingData:
+                # Play the voice data if DATA Matched:
+                if line1 == line2:
+                        M.system("xdg-open /home/johnmelodyme/Documents/API/AI/Sounds/123.mp3")
+                        break
+                else:
+                        print("Please try again!")
+                        M.system("xdg-open /home/johnmelodyme/Documents/API/AI/Sounds/false.mp3")   
+                        break
+                break
+        break
