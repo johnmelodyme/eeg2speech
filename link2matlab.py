9a173c50dfa6098e97a85b34138a1a1d6f50bd70
John Melody Melissa <Johnmelodyme@yandex.com>
2019-05-24 14:14:15 +0800

    Delete link2matlab.py

--- link2matlab.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-#By John MElody ME 
-import os as F
-import webbrowser as W
-
-F.system('echo This is a Matlab link:  ')
-matlab = "https://octave-online.net/"
-W.open(matlab)
